690a92173dd15bd282be6bd6897763c62c5fb909
karanonymous <karan.edu20@gmail.com>
2026-02-21 03:51:25 +0000

    Sync LeetCode submission Runtime - 5 ms (76.51%), Memory - 45.9 MB (73.21%)

--- solutions/0009-palindrome-number/solution.java
+++ solutions/0009-palindrome-number/solution.java
@@ -1,12 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int temp=x;
-        int neew=0;
-        while(temp>0){
-            int rem=temp%10;
-            neew=(neew*10)+rem;
-            temp=temp/10; 
-        }
-        return x==neew;
+
+       if(x<0 || (x!=0 && x%10==0)){
+        return false;
+       }
+
+       int rev=0;
+       
+
+       while(x>rev){
+        rev = rev * 10 + x%10;
+        x=x/10;
+       }
+
+       //
+       return (rev==x) || (x==rev/10);
     }
 }
+
