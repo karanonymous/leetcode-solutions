b67f0d74f224acf6265e5a9d705b57ddcdaf14fe
karanonymous <karan.edu20@gmail.com>
2026-07-20 09:22:34 +0530

    Time: 3 ms (99.88%) | Memory: 46.2 MB (90.58%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,6 +1,6 @@
 class Solution {
     public String intToRoman(int num) {
-        String res="";
+        StringBuilder res=new StringBuilder();
         String[] st={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
 
@@ -9,11 +9,11 @@ class Solution {
             //value corresponding string ko res me daalo
             // num-value
             while(num>=val[i]){
-                res+=st[i];
+                res.append(st[i]);
                 num-=val[i];
             }
         } 
 
-        return res;
+        return res.toString();
     }
 }
